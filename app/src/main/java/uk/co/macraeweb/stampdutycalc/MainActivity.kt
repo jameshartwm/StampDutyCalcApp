aa2371ec6a34b7075b545d99db0c5ee7179d30df
James Hart <jameshartwm@gmail.com>
2022-05-03 23:52:34 +0100

    Added basic calculation logic

--- app/src/main/java/uk/co/macraeweb/stampdutycalc/MainActivity.kt
+++ app/src/main/java/uk/co/macraeweb/stampdutycalc/MainActivity.kt
@@ -2,10 +2,25 @@ package uk.co.macraeweb.stampdutycalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun calculateStandard (view: View) {
+        val textEdit = findViewById<EditText>(R.id.standard);
+
+        try {
+            val price = textEdit.text.toString().toInt();
+            val result = price * 2;
+            findViewById<TextView>(R.id.result).setText(result.toString());
+        } finally {
+
+        }
     }
 }
